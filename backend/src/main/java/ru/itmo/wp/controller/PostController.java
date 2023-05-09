2a00ac451b84025781348d13e6caa81eebff991e
George Burunsuzian <goshabur2003@gmail.com>
2023-05-09 12:13:25 +0300

    initial commit

---
+++ backend/src/main/java/ru/itmo/wp/controller/PostController.java
@@ -0,0 +1,36 @@
+package ru.itmo.wp.controller;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.itmo.wp.domain.Post;
+import ru.itmo.wp.exception.ValidationException;
+import ru.itmo.wp.form.PostForm;
+import ru.itmo.wp.service.PostService;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/1")
+public class PostController {
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("posts")
+    public List<Post> findPosts() {
+        return postService.findAll();
+    }
+
+    @PostMapping("posts/create")
+    public void register(@RequestBody @Valid PostForm postForm,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new ValidationException(bindingResult);
+        }
+
+        postService.register(postForm);
+    }
+}
---
+++ frontend/src/components/main/Index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Post v-for="post in Object.values(this.posts).sort((a, b) => b.id - a.id)"
+              :post="post" :users="users" :key="post.id"/>
+    </div>
+</template>
+
+<script>
+import Post from "../page/Post";
+export default {
+    name: "Index",
+    components: {Post},
+    props: ["posts", "users"]
+}
+</script>
+
+<style scoped>
+
+</style>
